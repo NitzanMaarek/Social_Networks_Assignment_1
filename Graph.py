0209f55ab906ca61be296174f23e868abaf560f6
NitzanMaarek <maarekn@post.bgu.ac.il>
2019-11-11 15:07:57 +0200

    graph load_graph works

--- Graph.py
+++ Graph.py
@@ -5,6 +5,7 @@ class Graph:
 
     def __init__(self):
         self.number_of_nodes = 0
+        self.graph_dict = {}
 
     def load_graph(self, path):
         """
@@ -14,8 +15,14 @@ class Graph:
         """
         if path is None or not os.path.isfile(path):
             return 'Path not found.'
-        df = pd.read_csv(path)
-
-
+        df = pd.read_csv(path, header=None)
+        for index, row in df.iterrows():
+            source = row[0]
+            dest = row[1]
+            if source not in self.graph_dict.keys():
+                self.graph_dict[source] = set()        #Initializing set
+            self.graph_dict[source].add(dest)
 
 if __name__ == '__main__':
+    graph = Graph()
+    graph.load_graph(r'C:\Users\nitsa\Desktop\Wikipedia_votes.csv')
