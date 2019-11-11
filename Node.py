27c42a4b557bd5290a006a7a6a5b0172cb1f1bda
chenyanai <chenyanai@gmail.com>
2019-11-11 14:43:05 +0200

    Added Node class

---
+++ Node.py
@@ -0,0 +1,17 @@
+"""
+Node class that represents a single Node in the graph.
+Each Node has it's current page rank from the last page rank calculation and a list of its neighbors.
+"""
+
+class Node:
+
+    def __init__(self, neighbors, name):
+        self.neighbors = neighbors
+        self.page_rank = -1
+        self.name = name
+
+    def set_page_rank(self, page_rank):
+        self.page_rank = page_rank
+
+    def get_page_rank(self):
+        return self.page_rank
